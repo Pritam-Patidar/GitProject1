c0dff711f7232bf3fb597046976433b9407989ae
Pritam-Patidar <107365361+Pritam-Patidar@users.noreply.github.com>
2022-06-14 11:59:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/userdataapplication/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.example.userdataapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+    return view;
+ }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        signupBtn.setOnClickListener(){
+
+        }
+    }
+
+}
+
--- app/src/main/java/com/example/userdataapplication/MainActivity.kt
+++ app/src/main/java/com/example/userdataapplication/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentLogin = LoginFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragmentLogin).commit()
+
     }
-}
+
+//    override fun passDataCom(editTextInput: String) {
+//        val bundle = Bundle()
+//        bundle.putString("Message", editTextInput)
+//        val transaction = this.supportFragmentManager.beginTransaction()
+//        val fragmentSignup = SignupFragment()
+//        fragmentSignup.arguments = bundle
+//
+//        transaction.replace(R.id.frameLayout,fragmentSignup)
+//        transaction.commit()
+    }
